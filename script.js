b6cb70e8cd0049c2763f3641fb1c791c1abe8f7a
Hrutesh Pujar <hruteshpujar07@gmail.com>
2026-09-17 01:43:22 +0530

    Update responsive website and client logos

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const fs = require('fs'); const html = fs.readFileSync('index.html', 'utf8'); const m = html.match(/marquee-content-rtl[\\s\\S]*?<\/div>/); console.log('RTL:', m ? m[0].split('<img').length - 1 : 0); const m2 = html.match(/marquee-content-ltr[\\s\\S]*?<\/div>/); console.log('LTR:', m2 ? m2[0].split('<img').length - 1 : 0);
+const fs = require('fs');
+
+const raw = fs.readFileSync('index.html', 'utf8');
+let html = raw;
+
+// If bundled in __bundler/template, extract the template string
+const match = raw.match(/<script type="__bundler\/template">([\s\S]*?)<\/script>/i);
+if (match) {
+  try {
+    html = JSON.parse(match[1]);
+  } catch (e) {
+    // fallback to raw
+  }
+}
+
+const m = html.match(/class="marquee-content-rtl"[\s\S]*?<\/div>\s*<\/div>/);
+console.log('RTL:', m ? (m[0].match(/<img/g) || []).length : 0);
+
+const m2 = html.match(/class="marquee-content-ltr"[\s\S]*?<\/div>\s*<\/div>/);
+console.log('LTR:', m2 ? (m2[0].match(/<img/g) || []).length : 0);
